10dbb2be2796edcc4e893401e28d0577f2070923
luciano-repetti <lucianorepetti2020@gmail.com>
2023-12-11 01:18:16 -0300

    fix error api

--- server.js
+++ server.js
@@ -5,31 +5,14 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 const app = express();
 app.use(cors());
 
-// Configuración de CORS para permitir solicitudes desde cualquier origen
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Permitir cualquier origen
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
-
 // Configuración de proxy para redirigir solicitudes a CleanURI
-app.use('/api/v1', (req, res, next) => {
-  // Manejar solicitudes OPTIONS manualmente
-  if (req.method === 'OPTIONS') {
-    res.sendStatus(200);
-  } else {
-    // Para otras solicitudes, usar el proxy
-    createProxyMiddleware({
-      target: 'https://cleanuri.com',
-      changeOrigin: true,
-      pathRewrite: {
-        '^/api/v1': '/api/v1',
-      },
-    })(req, res, next);
-  }
-});
+app.use('/api/v1', createProxyMiddleware({
+  target: 'https://cleanuri.com',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/api/v1': '/api/v1',
+  },
+}));
 
 const PORT = 3001;
 app.listen(PORT, () => {
